def8801f82463759afad0d18238f9c6fe17348a3
glavadoj01 <gonzalolj@hotmail.es>
2026-01-07 11:32:19 +0100

    add default components structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Bienvenida } from './pages/bienvenida/bienvenida';
+import { Libros } from './pages/catalogos/libros/libros';
+import { Eventos } from './pages/catalogos/eventos/eventos';
+import { Listas } from './pages/catalogos/listas/listas';
+import { Evento } from './pages/detalle/evento/evento';
+import { Libro } from './pages/detalle/libro/libro';
+import { Lista } from './pages/detalle/lista/lista';
+import { Auth } from './pages/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth', component: Auth },
+    { path: 'bienvenida', component: Bienvenida },
+    { path: 'catalogos/eventos', component: Eventos },
+    { path: 'catalogos/libros', component: Libros },
+    { path: 'catalogos/listas', component: Listas },
+    { path: 'detalle/evento', component: Evento },
+    { path: 'detalle/libro', component: Libro },
+    { path: 'detalle/lista', component: Lista },
+    { path: '**', redirectTo: 'bienvenida' }
+];
---
+++ src/app/pages/catalogos/listas/listas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listas',
+  imports: [],
+  templateUrl: './listas.html',
+  styleUrl: './listas.css'
+})
+export class Listas {
+
+}
