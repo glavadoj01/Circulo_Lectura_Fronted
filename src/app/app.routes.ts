6db21a1d343e5d6846fca499e69cf50b7e27ce48
glavadoj01 <gonzalolj@hotmail.es>
2026-01-08 13:02:28 +0100

    minnor orders fixed & add  ¿temps? descriptions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
+import { Auth } from './pages/auth/auth';
 import { Bienvenida } from './pages/bienvenida/bienvenida';
-import { Libros } from './pages/catalogos/libros/libros';
 import { Eventos } from './pages/catalogos/eventos/eventos';
+import { Libros } from './pages/catalogos/libros/libros';
 import { Listas } from './pages/catalogos/listas/listas';
 import { Evento } from './pages/detalle/evento/evento';
 import { Libro } from './pages/detalle/libro/libro';
 import { Lista } from './pages/detalle/lista/lista';
-import { Auth } from './pages/auth/auth';
 
 export const routes: Routes = [
     { path: 'auth', component: Auth },
@@ -17,5 +17,8 @@ export const routes: Routes = [
     { path: 'detalle/evento', component: Evento },
     { path: 'detalle/libro', component: Libro },
     { path: 'detalle/lista', component: Lista },
-    { path: '**', redirectTo: 'bienvenida' }
+    { path: '', redirectTo: 'bienvenida', pathMatch: 'full' }, /* Ruta por defecto www.webapp.es */
+    { path: '**', redirectTo: 'bienvenida' } /* Ruta para cuando no existe la ruta www.webapp.es/loquesea */
+    /* Realmente la default es redundante, ya que está la cualquiera **; que atraparia la ruta vacia igualmente, 
+        pero se puede aprovechar para incluir algun codigo de error => WIP */
 ];
