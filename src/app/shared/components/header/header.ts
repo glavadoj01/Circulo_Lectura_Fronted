54834bcacd7b521cf8d62d12074989719726be2b
glavadoj01 <gonzalolj@hotmail.es>
2026-01-12 19:57:06 +0100

    add daisyUI and become theme's desings and toggle buttom

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -8,5 +8,11 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.css'
 })
 export class Header {
-
+    toggleTheme() {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme') || 'dark';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    html.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    }
 }
