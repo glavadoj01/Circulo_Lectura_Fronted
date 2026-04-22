e9a6a2dda924f2a690048828579249fc06b8d2e2
glavadoj01 <gonzalolj@hotmail.es>
2026-04-22 13:14:31 +0200

    Proyecto nuevas paginas

--- src/app/pages/perfil/perfil.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-perfil',
-    imports: [],
-    templateUrl: './perfil.html',
-})
-export class Perfil {}
---
+++ src/app/pages/perfilUsuario/perfilUsuario.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-perfil-usuario',
+    imports: [],
+    templateUrl: './perfilUsuario.html',
+})
+export class PerfilUsuario {}
